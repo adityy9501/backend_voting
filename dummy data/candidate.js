ae8aafe324f8e6b297a8a5f18c2a673fa98081c4
Aditya <adityahurdees12@gmail.com>
2024-03-12 08:36:22 +0530

    first version

---
+++ dummy data/candidate.js
@@ -0,0 +1,19 @@
+[
+    {
+        "name": "Nitin Gadkari",
+        "party": "BJP",
+        "age": 71
+    },
+    {
+        "name": "Rahul Gandhi",
+        "party": "INC",
+        "age": 51
+    },
+    {
+        "name": "Arvind Kejriwal",
+        "party": "AAP",
+        "age": 53
+    },
+   
+
+]
